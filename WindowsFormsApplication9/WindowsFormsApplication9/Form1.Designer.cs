1e05dc2ee4a5ca36f2377c70cd7ea1e2e7967c7e
Ali Raza <alir9996@gmail.com>
2019-10-02 03:26:42 +0500

    VP lab task

---
+++ WindowsFormsApplication9/WindowsFormsApplication9/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApplication9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Vowels";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(147, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Alphabets";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(269, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Size";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 142);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Lines";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(147, 142);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Characters";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(269, 142);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Words";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 261);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApplication9/WindowsFormsApplication9/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //OpenFileDialog f = new OpenFileDialog();
+        string filedata = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    filedata = File.ReadAllText(file);
+                    MessageBox.Show("Reading file");
+
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Invalid");
+                }
+            } 
+       }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            for (int i = 0; i < filedata.Length; i++)
+            {
+                if (filedata[i] == 'A' || filedata[i] == 'E' || filedata[i] == 'I' || filedata[i] == 'O' || filedata[i] == 'U' || filedata[i] == 'a' || filedata[i] == 'e' || filedata[i] == 'i' || filedata[i] == 'o' || filedata[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show("Vowels : " + count);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int count = 1;
+            for (int i = 0; i < filedata.Length; i++)
+            {
+                if (filedata[i] == '\n')
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show("Lines : " + count);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+             int count = 0;
+            for (int i = 0; i < filedata.Length; i++)
+            {
+                if((filedata[i]>='A' && filedata[i] <='Z') || ((filedata[i])>='a' && filedata[i] <='z'))
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show("Alphabets : " + count);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            for (int i = 0; i < filedata.Length; i++)
+            {
+                if (filedata[i] == ',' || filedata[i] == '.' || filedata[i] == '*' || filedata[i] == '`' || filedata[i] == '~' || filedata[i] == '!' || filedata[i] == '@' || filedata[i] == '#' || filedata[i] == '$' || filedata[i] == '%' || filedata[i] == '^' || filedata[i] == '&' || filedata[i] == '(' || filedata[i] == ')' || filedata[i] == '-' || filedata[i] == '_' || filedata[i] == '+' || filedata[i] == '=' || filedata[i] == '/' || filedata[i] == '?')
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show("Special Characters : " + count);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int count = 1;
+            for (int i = 0; i < filedata.Length; i++)
+            {
+                if (filedata[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show("Words : " + count);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("File Size : " + filedata.Length);
+        }
+    }
+}
